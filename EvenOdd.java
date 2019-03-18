d8509f58f005fb61a667088c9786b957f74d2a13
NadeemAhmad007 <44241167+NadeemAhmad007@users.noreply.github.com>
2019-03-18 14:06:06 +0100

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -6,10 +6,18 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+		int a = scan.nextInt();
+		for(int i =0;i<a; i++ ) {
+		if(i%2!=0) {
+			System.out.print("\n"+i);
+			
+		}
+//		else {
+//			System.out.println(" Odd  " +a);
+//		}
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
-		
+		}
 	}
 	
 }
