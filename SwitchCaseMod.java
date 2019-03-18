bc0e5c36d4460074551138ef524d4609cdda11d9
NadeemAhmad007 <44241167+NadeemAhmad007@users.noreply.github.com>
2019-03-18 14:05:37 +0100

    Update SwitchCaseMod.java

--- SwitchCaseMod.java
+++ SwitchCaseMod.java
@@ -12,6 +12,47 @@ public class SwitchCaseMod {
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		
+		if(0<b && b<10){
+			switch(a%b) {
+			case 0:
+				System.out.println("Zero");
+				break;
+			case 1:
+				System.out.println("One");	
+				break;
+			case 2:
+				System.out.println("Two");
+				break;
+			case 3:
+				System.out.println("Three");
+				break;
+			case 4:
+				System.out.println("Four");
+				break;
+			case 5:
+				System.out.println("Five");
+				break;
+			case 6:
+				System.out.println("Six");
+				break;
+			case 7:
+				System.out.println("Seven");
+				break;
+			case 8:
+				System.out.println("Eight");
+				break;
+			case 9:
+				System.out.println("Nine");
+				break;
+			
+			}
+			
+		}else 
+		{ 
+	
+			System.out.println("Wrong Input");
+		}
+		
 		/*
 		 * Todo: First check if 0<b<10.If this condition does not hold, print a little
 		 * output informing the user that he or she enteres wrong values.
